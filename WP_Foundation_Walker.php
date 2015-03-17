a12d1b7e47f768c895dccdee21aba18feeab974d
peterbooker <peter@kebopowered.com>
2015-03-17 13:40:45 +0000

    initial version of walker and helper for admin bar compatibility

--- WP_Foundation_Walker.php
+++ WP_Foundation_Walker.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP Foundation Walker
  * Plugin URI:  https://github.com/PeterBooker/wp-foundation-walker
- * Description: Makes the default Menu system compatible with Foundation's Top Bar. (http://foundation.zurb.com/docs/components/topbar.html)
+ * Description: Makes the default Menu system compatible with Foundation's Top Bar. Can be used as a Must Use (MU) plugin or included in a theme directly.
  * Version:     1.0
  * Author:      Peter Booker
  * Author URI:  http://www.peterbooker.com
@@ -15,84 +15,132 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Add Last-Modified and Cache-Control Headers to WP Responses
+ * Custom Menu Walker for Foundation's Top Bar menu.
+ * Foundation Docs - http://foundation.zurb.com/docs/components/topbar.html
  */
 class WP_Foundation_TopBar extends Walker_Nav_Menu {
-	
-    function display_element( $element, &$children_elements, $max_depth, $depth=0, $args, &$output )
-    {
-        $element->has_children = !empty( $children_elements[$element->ID] );
-        if(!empty($element->classes)){
+
+    /*
+     * Add Top Bar specific CSS classes to menu items
+     */
+    function display_element( $element, &$children_elements, $max_depth, $depth = 0, $args, &$output ) {
+
+        $element->has_children = ! empty( $children_elements[ $element->ID ] );
+
+        if ( ! empty( $element->classes ) ) {
+
             $element->classes[] = ( $element->current || $element->current_item_ancestor ) ? 'active' : '';
             $element->classes[] = ( $element->has_children ) ? 'has-dropdown' : '';
+
         }
+
         parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
+
     }
+
+    /*
+     * The start of the menu
+     */
     function start_lvl( &$output, $depth = 0, $args = array() ) {
-        $indent = str_repeat("\t", $depth);
+
+        $indent = str_repeat( "\t", $depth );
         $output .= "\n$indent<ul class=\"sub-menu dropdown\">\n";
+
     }
-    function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0)
-    {
+
+    /*
+     * The end of the menu
+     */
+    function end_lvl( &$output, $depth = 0, $args = array() ) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "$indent</ul>\n";
+    }
+
+    /*
+     * The start of each menu item
+     */
+    function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+
         $item_output = '';
         $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
         $output .= ( $depth == 0 ) ? '<li class="divider"></li>' : '';
-        $class_names = $value = '';
-        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+
+        $classes = empty( $item->classes ) ? array() : ( array ) $item->classes;
         $classes[] = 'menu-item-'. $item->ID;
         $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
         $class_names = ' class="' . esc_attr( $class_names ) . '"';
-        $output.= $indent.'<li id="menu-item-'. $item->ID.'" '.$class_names.'>';
-        if ( empty( $item->title ) && empty( $item->url ))
-        {
-            $item->url = get_permalink($item->ID);
+        $output .= $indent . '<li id="menu-item-' . $item->ID . '" ' . $class_names . '>';
+
+        if ( empty( $item->title ) && empty( $item->url ) )  {
+
+            $item->url = get_permalink( $item->ID );
             $item->title = $item->post_title;
-            $attributes = $this->attributes($item);
-            $item_output .= '<a'. $attributes .'>';
+            $attributes = $this->build_attributes( $item );
+            $item_output .= '<a' . $attributes . '>';
             $item_output .= apply_filters( 'the_title', $item->title, $item->ID );
             $item_output .= '</a>';
-        }
-        else
-        {
-            $attributes = $this->attributes($item);
+
+        } else {
+
+            $attributes = $this->build_attributes( $item );
             $item_output = $args->before;
-            $item_output .= '<a'. $attributes .'>';
+            $item_output .= '<a' . $attributes . '>';
             $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
             $item_output .= '</a>';
             $item_output .= $args->after;
+
         }
+
         $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args, $id );
+
     }
-    private function attributes($item)
-    {
-        $attributes = ! empty( $item->attr_title ) ? ' title="' . esc_attr( $item->attr_title ) .'"' : '';
-        $attributes .= ! empty( $item->target ) ? ' target="' . esc_attr( $item->target ) .'"' : '';
-        $attributes .= ! empty( $item->xfn ) ? ' rel="' . esc_attr( $item->xfn ) .'"' : '';
-        $attributes .= ! empty( $item->url ) ? ' href="' . esc_attr( $item->url ) .'"' : '';
+
+    /*
+     * The end of each menu item
+     */
+    function end_el( &$output, $item, $depth = 0, $args = array() ) {
+        $output .= "</li>\n";
+    }
+
+    private function build_attributes( $item ) {
+
+        $attributes = '';
+        $attributes .= ! empty( $item->attr_title ) ? ' title="' . esc_attr( $item->attr_title ) . '"' : '';
+        $attributes .= ! empty( $item->target ) ? ' target="' . esc_attr( $item->target ) . '"' : '';
+        $attributes .= ! empty( $item->xfn ) ? ' rel="' . esc_attr( $item->xfn ) . '"' : '';
+        $attributes .= ! empty( $item->url ) ? ' href="' . esc_attr( $item->url ) . '"' : '';
+
         return $attributes;
+
     }
-    public static function items_default_wrap($menu_text) {
-        /**
-         * Set default menu for menus not yet linked to theme location
-         * Method courtesy of robertomatute - https://github.com/roots/roots/issues/939
-         */
-        return str_replace('<ul>', '<ul class="right">', $menu_text);
-    }
-    public static function items_remove_default_wrapper()
-    {
-        /**
-         * Remove default div wrapper around ul element
-         */
-        ?>
-        <script type="text/javascript">
-            jQuery(document).ready(function($){
-                var default_nav = $(".top-bar-section > div > ul");
-                if(default_nav.parent("div").hasClass("right") === true){
-                    default_nav.unwrap();
-                }
-            });
-        </script>
-    <?php
-    }
+
 }
-add_filter( 'wp_page_menu', array('F5_TOP_BAR_WALKER','items_default_wrap'));
+
+/**
+ * Helper Functions to support Compatibility
+ */
+
+/*
+ * If using Top Bar at the top of the page, this ensures that it sits under the WordPress Admin Bar when it is active.
+ */
+if ( ! function_exists( 'wp_foundation_admin_bar_fix' ) ) {
+
+    function wp_foundation_admin_bar_fix() {
+
+        if ( ! is_admin() && is_admin_bar_showing() ) {
+
+            remove_action( 'wp_head', '_admin_bar_bump_cb' );
+
+            $output = '<style type="text/css">' . '\n\t';
+            $output .= 'body.admin-bar { padding-top: 46px; }' . '\n';
+            $output .= '@media ( min-width: 780px ) { body.admin-bar { padding-top: 32px; } }' . '\n';
+            $output .= '</style>' . '\n';
+
+            echo $output;
+
+        }
+
+    }
+    add_action( 'wp_head', 'wp_foundation_admin_bar_fix' );
+
+}
