2bdc21b657ddb8db70e101e46ec6c785e54195e7
peterbooker <peter@kebopowered.com>
2015-03-28 20:11:55 +0000

    improved description

--- WP_Foundation_Walker.php
+++ WP_Foundation_Walker.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP Foundation Walker
  * Plugin URI:  https://github.com/PeterBooker/wp-foundation-walker
- * Description: A custom WordPress Menu Walker compatible with Foundation's Top Bar. Can be used as a Must Use (MU) plugin or included in a Theme directly.
+ * Description: A custom WordPress Menu Walker compatible with the Foundation 5 Top Bar. Can be used as a Must Use (MU) plugin or included in a Theme directly.
  * Version:     1.1
  * Author:      Peter Booker
  * Author URI:  http://www.peterbooker.com
